d7b1483697e334c6a9b849e3eccd989100dbf0b8
nabillnuqman <54606977+nabillnuqman@users.noreply.github.com>
2020-04-09 11:28:59 +0700

    Delete question3week28.py

--- question3week28.py
+++
@@ -1,4 +0,0 @@
-# Write a program that accepts a comma separated sequence of words as input and prints the words in a comma-separated sequence after sorting them alphabetically.
-items = input("Please input words separated by comma's")
-words = [word for word in items.split(",")]
-print(",".join(sorted(list(set(words)))))
